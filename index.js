b793f87b17bd0fbd09d8f3edd4b140f196bf9ba7
tarkesh2shar <tarkesh2shar@gmail.com>
2020-04-15 20:05:37 +0530

    error handling

--- index.js
+++ index.js
@@ -13,16 +13,30 @@ app.use('/api', proxy(process.env.API_SERVER))
 app.get('*', (req, res) => {
 	const store = createStore(req)
 	//------------------------>  Load all the data Here  <------------------------//
-	const promises = matchRoutes(Routes, req.path).map(({ route }) => {
-		return route.loadData ? route.loadData(store) : null
+	const promises = matchRoutes(Routes, req.path)
+		.map(({ route }) => {
+			return route.loadData ? route.loadData(store) : null
+		})
+		.map(promise => {
+			if (promise) {
+				return new Promise((resolve, reject) => {
+					promise.then(resolve).catch(resolve)
+				})
+			}
+		})
+	Promise.all(promises).then(_ => {
+		const context = {}
+		const content = Renderer(req, store, context)
+		//if we tend to redirect to some other place //
+		//usually from <Redirect> Tag from react-router-dom
+		if (context.url) {
+			return res.redirect(301, context.url)
+		}
+		if (context.notFound) {
+			res.status(404)
+		}
+		res.send(content)
 	})
-	Promise.all(promises)
-		.then(data => {
-			res.send(Renderer(req, store))
-		})
-		.catch(e => {
-			console.log(e)
-		})
 })
 app.listen(4000, () => {
 	console.log('Listening at port 4000')
